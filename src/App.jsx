34d1d6481fdf3aae68fdcffa53cc53f38505b389
Gokhan Sigircik <gokhansigircik@hotmail.com>
2023-01-11 18:38:35 -0500

    HelloDojo

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Dojo!</h1>
+        <p>Things i need to do:</p>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a Marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App
